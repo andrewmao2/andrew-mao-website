e47cd3a6677cc7806d73035199d5b8e08198ef79
Andrew Mao <andrewmao2@gmail.com>
2024-05-15 17:02:06 -0400

    header component and toggling the navbar completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,54 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
-  return (
-    <header className="header">
-        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"></link>
-        <nav className="nav container">
-            <a href="index.html" className="nav__logo">Mao</a>
-                <div className="nav__menu">
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <header className="header">
+            <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"></link>
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">Mao</a>
+                <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <i class="uil uil-estate nav__icon"></i> Home
+                            <a href="#home" className="nav__link active-link">
+                                <i className="uil uil-estate nav__icon"></i> Home
                             </a>
                         </li>
-
                         <li className="nav__item">
                             <a href="#about" className="nav__link">
                                 <i className="uil uil-user nav__icon"></i> About
                             </a>
                         </li>
-
                         <li className="nav__item">
                             <a href="#skills" className="nav__link">
                                 <i className="uil uil-file nav__icon"></i> Skills
                             </a>
                         </li>
-
                         <li className="nav__item">
                             <a href="#services" className="nav__link">
                                 <i className="uil uil-briefcase-alt nav__icon"></i> Services
                             </a>
                         </li>
-
                         <li className="nav__item">
                             <a href="#portfolio" className="nav__link">
                                 <i className="uil uil-scenery nav__icon"></i> Portfolio
                             </a>
                         </li>
-
                         <li className="nav__item">
                             <a href="#contact" className="nav__link">
                                 <i className="uil uil-message nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
+                    <div className="nav__close" onClick={() => setToggle(!toggle)}>
+                        <i className="uil uil-times"></i>
+                    </div>
                 </div>
-        </nav>
-    </header>
-  )
+                <div className="nav__toggle" onClick={() => setToggle(!toggle)}>
+                    <i className="uil uil-apps"></i>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Header;
