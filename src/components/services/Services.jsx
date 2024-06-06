53f8be9221fbc3a163a7c1fdc54ed2f6e176caf1
Andrew Mao <andrewmao2@gmail.com>
2024-06-05 21:55:44 -0700

    services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import "./services.css";
+
 
 const Services = () => {
   return (
@@ -8,11 +10,151 @@ const Services = () => {
         <div className="services__container container grid">
           <div className="services__content">
             <div>
-              <i className="uil uil services__icon"></i>
-              <h3 className="services__title"></h3>
+              <i className="uil uil-web-grid services__icon"></i>
+              <h3 className="services__title">
+                Product <br /> designer
+              </h3>
             </div>
+ 
+            <span className="services__button">View Mor <i className="uil uil-arrow-right services__botton-icon"></i></span>
 
-            <span className="services__button"></span>
+            <div className="services__modal">
+              <div className="services__modal-content">
+                 <i className="uil uil-times services__modal-close"></i>
+                 <h3 className="services__modal-title">
+                 Product <br /> designer
+                 </h3>
+                 <p className="services__modal-description">
+                  filler filler filler abcdefw
+                 </p>
+
+                 <u1 className="services__modal-services.grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+                 </u1>
+              </div>
+            </div>
+          </div>
+          <div className="services__content">
+            <div>
+              <i className="uil uil-arrow services__icon"></i>
+              <h3 className="services__title">
+              Product <br /> designer
+              </h3>
+            </div>
+ 
+            <span className="services__button">View More <i className="uil uil-arrow-right services__botton-icon"></i></span>
+
+            <div className="services__modal">
+              <div className="services__modal-content">
+                 <i className="uil uil-times services__modal-close"></i>
+                 <h3 className="services__modal-title">
+                 Product <br /> designer v2
+                 </h3>
+                 <p className="services__modal-description">
+                  filler filler filler abcdefw
+                 </p>
+
+                 <u1 className="services__modal-services.grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+                 </u1>
+              </div>
+            </div>
+          </div>
+
+          <div className="services__content">
+            <div>
+              <i className="uil uil-edit services__icon"></i>
+              <h3 className="services__title">
+              Product <br /> designer
+              </h3>
+            </div>
+ 
+            <span className="services__button">View More <i className="uil uil-arrow-right services__botton-icon"></i></span>
+
+            <div className="services__modal">
+              <div className="services__modal-content">
+                 <i className="uil uil-times services__modal-close"></i>
+                 <h3 className="services__modal-title">
+                 Product <br /> designer v2
+                 </h3>
+                 <p className="services__modal-description">
+                  filler filler filler abcdefw
+                 </p>
+
+                 <u1 className="services__modal-services.grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p className="services__modal-info"> More filler more filler </p>
+                  </li>
+                 </u1>
+              </div>
+            </div>
           </div>
         </div>
     </section>
